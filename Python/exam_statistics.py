58400c8cac287c9b5a9c981cffc2a48388df3d26
James Payne <jpayne001@me.com>
2016-01-31 18:20:15 +1100

    Added print grades function.

--- Python/exam_statistics.py
+++ Python/exam_statistics.py
@@ -1,7 +1,18 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
+# --Print all the grades--
+
 def print_grades(grades):
     for grade in grades:
         print grade
 
 # print_grades(grades)
+
+# --Get the sum of the grades/scores--
+
+def grades_sum(scores):
+    total = 0
+    for score in scores:
+        total += score
+    return total
+# print grades_sum(grades)
